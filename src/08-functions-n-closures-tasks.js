49109f36d0830dee90d77e36951e30c03b92eeb2
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-07-03 12:58:50 +0300

    feat: add solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -177,8 +177,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let i = 0;
+  return function x() {
+    const res = i + startFrom;
+    i += 1;
+    return res;
+  };
 }
 
 
